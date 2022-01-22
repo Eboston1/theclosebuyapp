adaaa9da251747a160fc1a8ebf73c652bfdc58a6
Eboston1 <epless88@protonmail.com>
2022-01-22 17:26:36 -0600

    deploy commit

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import UserLocation from '@/pages/UserLocation'
+import CloseBuy from '@/pages/CloseBuy'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: UserLocation
+    },
+    {
+      path: '/close-buy',
+      component: CloseBuy
+    }
+  ]
+})
