8978daa1b837eabe959176aa9e38a7b04a41b6e7
Eboston1 <epless88@protonmail.com>
2022-01-23 17:12:58 -0600

    update indexjs

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import UserLocation from 'src/pages/UserLocation'
-import CloseBuy from 'src/pages/CloseBuy'
+import UserLocation from '@/pages/UserLocation'
+import CloseBuy from '@/pages/CloseBuy'
 
 Vue.use(Router)
 
