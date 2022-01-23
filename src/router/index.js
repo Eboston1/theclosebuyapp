652717cf9397a0701e289d32c8bd5e60518bdd16
Eboston1 <epless88@protonmail.com>
2022-01-23 14:15:16 -0600

    change index3

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import UserLocation from './src/pages/UserLocation'
-import CloseBuy from './src/pages/CloseBuy'
+import UserLocation from './src/pages/UserLocation.Vue'
+import CloseBuy from './src/pages/CloseBuy.Vue'
 
 Vue.use(Router)
 
